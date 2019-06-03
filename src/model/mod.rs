5c9109eb00d21ada342a23fa95647b73faaeb7bd
Lin Yinfeng <lin.yinfeng@outlook.com>
2019-06-03 13:27:50 +0800

    Add full model struct

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -2,3 +2,9 @@ pub mod board;
 pub mod common;
 pub mod stateful;
 pub mod stateless;
+
+#[derive(Clone, Debug)]
+pub struct Model {
+    pub stateless: stateless::Model,
+    pub stateful: stateful::Model,
+}
