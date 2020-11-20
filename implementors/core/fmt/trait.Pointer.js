6adf2d863a21196fcdf8dfe074947ba678a1c9f5
linyinfeng <linyinfeng@users.noreply.github.com>
2020-11-20 07:26:12 +0000

    deploy: 6407517f9346f27c7bc6b4469a51d26131329efc

--- implementors/core/fmt/trait.Pointer.js
+++ implementors/core/fmt/trait.Pointer.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
-implementors["crossbeam_epoch"] = [{"text":"impl&lt;T&gt; Pointer for Atomic&lt;T&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'g, T&gt; Pointer for Shared&lt;'g, T&gt;","synthetic":false,"types":[]}];
+implementors["crossbeam_epoch"] = [{"text":"impl&lt;T:&nbsp;?Sized + Pointable&gt; Pointer for Atomic&lt;T&gt;","synthetic":false,"types":[]},{"text":"impl&lt;T:&nbsp;?Sized + Pointable, '_&gt; Pointer for Shared&lt;'_, T&gt;","synthetic":false,"types":[]}];
 implementors["env_logger"] = [{"text":"impl&lt;'a, T:&nbsp;Pointer&gt; Pointer for StyledValue&lt;'a, T&gt;","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/core/iter/traits/marker/trait.FusedIterator.js
+++ implementors/core/iter/traits/marker/trait.FusedIterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["crossbeam_channel"] = [{"text":"impl&lt;'a, T&gt; FusedIterator for Iter&lt;'a, T&gt;","synthetic":false,"types":[]},{"text":"impl&lt;T&gt; FusedIterator for IntoIter&lt;T&gt;","synthetic":false,"types":[]}];
+implementors["crossbeam_channel"] = [{"text":"impl&lt;T, '_&gt; FusedIterator for Iter&lt;'_, T&gt;","synthetic":false,"types":[]},{"text":"impl&lt;T&gt; FusedIterator for IntoIter&lt;T&gt;","synthetic":false,"types":[]}];
 implementors["mpi_traffic"] = [{"text":"impl FusedIterator for Indices","synthetic":false,"types":[]}];
 implementors["rand"] = [{"text":"impl&lt;D, R, T&gt; FusedIterator for DistIter&lt;D, R, T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;D: Distribution&lt;T&gt;,<br>&nbsp;&nbsp;&nbsp;&nbsp;R: Rng,&nbsp;</span>","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
